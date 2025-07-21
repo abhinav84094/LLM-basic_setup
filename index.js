99c0998fb460b005d41e5634228722b7643d7ded
Abhinav Prakash <155569593+abhinav84094@users.noreply.github.com>
2025-07-21 11:51:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { GoogleGenAI } from "@google/genai";
 import readlineSync from "readline-sync"
 import { text } from "stream/consumers";
 
-const ai = new GoogleGenAI({apiKey});
+const ai = new GoogleGenAI({APIKEY});
 
 const History = [];
 async function Chatting(userProblem) {
